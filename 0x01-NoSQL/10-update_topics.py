b70e9f99331f50228e52b3c2fbc84658b662281a
timmySpark <timmyspark1@gmail.com>
2024-08-27 10:08:34 +0000

    [feat] :- update school topics in collection

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Task 10. Change school topics'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''Change school topics
+        mongo_collection will be the pymongo collection object
+        name (string) will be the school name to update
+        topics (list of strings) will be the list of topics approached in the school
+    '''
+    mongo_collection.update_many(
+        {"name": name},
+        {'$set': {'topics': topics}}
+    )
