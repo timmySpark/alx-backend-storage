17ddb3d51a002251faac7f9ad4d249d4fd3dddb1
timmySpark <timmyspark1@gmail.com>
2024-08-27 10:15:22 +0000

    [fix] :- pycodestyle fixes

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,7 +6,8 @@ def update_topics(mongo_collection, name, topics):
     '''Change school topics
         mongo_collection will be the pymongo collection object
         name (string) will be the school name to update
-        topics (list of strings) will be the list of topics approached in the school
+        topics (list of strings) will be the list of topics
+                                            approached in the school
     '''
     mongo_collection.update_many(
         {"name": name},
