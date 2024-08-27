030c1e91177b08819350147b191d4d1cb1c3b8ff
timmySpark <timmyspark1@gmail.com>
2024-08-27 08:49:11 +0000

    [feat] :- lists all the document in a collection, return an empty list if doc are not present

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 8: Lists all documents in collection'''
+
+
+def list_all():
+    ''' List all document in a collection
+        Return an empty list if there is no document in the collection
+    '''
+    return [doc for doc in mongo_collection.find()]
