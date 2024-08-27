4dc16943c7d6021849d574f71120cda5d4216915
timmySpark <timmyspark1@gmail.com>
2024-08-27 10:07:48 +0000

    [feat] :- insert into a docuent and return the id

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Task 9. Insert a document in Python'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    ''' Insert a document in python
+        Return the new _id
+    '''
+    new_id = mongo_collection.insert_one(kwargs)
+    return new_id
